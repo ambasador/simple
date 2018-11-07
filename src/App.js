61cc397e3cb383449622270aea215efe15ec97be
Grzegorz Węgorzewski <bisect@me.com>
2018-11-07 15:45:59 +0100

    simple
    simple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+
+  handleClick = (e) => {
+    e.preventDefault();
+    const ele = this.refs.ele;
+    setTimeout(() => {
+        ele.classList.toggle('active');
+      }, 500);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+	     <button onClick={this.handleClick.bind(this)}>Click me</button>
+       <div className="test" ref="ele">test</div>
       </div>
     );
   }
